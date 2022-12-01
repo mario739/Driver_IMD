58dfe402fd6d2f429a0ccba29264df939ab3f6d1
mario739 <fernandoaguilar731010@gmail.com>
2022-12-01 19:20:52 -0400

    update app

--- app_aht10.c
+++ app_aht10.c
@@ -9,7 +9,7 @@
 #define TEMPERATURE(A)                       (int8_t) ((A *0.000191)-50) 
 int main(void){
     uint8_t cmd[3] = {AHT10_CMD_TRIGGER_MEASUREMENT,0x00,0x00};
-    char bufferRead[6]={0};
+    char buffer_read[6]={0};
     uint32_t data_humedity=0;
     uint8_t humedity=0;
     uint32_t data_temperature=0;
@@ -22,13 +22,12 @@ int main(void){
     else {
 
         write(my_dev,cmd,3);
-        read(my_dev,bufferRead,6);
-        data_humedity=(((uint32_t)bufferRead[1]<<16) | ((uint16_t)bufferRead[2]<<8) | (bufferRead[3]))>>4;
+        read(my_dev,buffer_read,6);
+        data_humedity=(((uint32_t)buffer_read[1]<<16) | ((uint16_t)buffer_read[2]<<8) | (buffer_read[3]))>>4;
         humedity=HUMEDITY(data_humedity);
         data_temperature=((uint32_t)(buffer_read[3] & 0x0F)<<16) | ((uint16_t) buffer_read[4]<<8)| buffer_read[5];
         temperature= TEMPERATURE(data_temperature);
-        printf("humedity %u temperature %i \n",humedity,temperature);
-        printf("hola\n ");
+        printf("humedad %u temperature %i \n",humedity,temperature);
         close(my_dev);
     }
     return 0;
