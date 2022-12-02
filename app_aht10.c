4b314b283c1820963102e36b89bc5281a44e2aba
mario739 <fernandoaguilar731010@gmail.com>
2022-12-01 21:57:31 -0400

    actualizacion de la app

--- app_aht10.c
+++ app_aht10.c
@@ -15,23 +15,26 @@ int main(void){
     uint32_t data_temperature=0;
     int8_t temperature=0;
 
-    int my_dev = open("/dev/aht10_dev", 0);
+    int my_dev = open("/dev/aht10_dev",O_RDWR);
+    
     if (my_dev < 0) {
         perror("Fail to open device file: /dev/mydev.");
     } 
-    else {
-
-        write(my_dev,cmd,3);
-        read(my_dev,buffer_read,6);
-        data_humedity=(((uint32_t)buffer_read[1]<<16) | ((uint16_t)buffer_read[2]<<8) | (buffer_read[3]))>>4;
-        humedity=HUMEDITY(data_humedity);
-        data_temperature=((uint32_t)(buffer_read[3] & 0x0F)<<16) | ((uint16_t) buffer_read[4]<<8)| buffer_read[5];
-        temperature= TEMPERATURE(data_temperature);
-        printf("humedad %u temperature %i \n",humedity,temperature);
-        close(my_dev);
+    else 
+    {
+        while (1)
+        {
+            write(my_dev,cmd,3);
+            read(my_dev,buffer_read,6);
+            data_humedity=(((uint32_t)buffer_read[1]<<16) | ((uint16_t)buffer_read[2]<<8) | (buffer_read[3]))>>4;
+            humedity=HUMEDITY(data_humedity);
+            data_temperature=((uint32_t)(buffer_read[3] & 0x0F)<<16) | ((uint16_t) buffer_read[4]<<8)| buffer_read[5];
+            temperature= TEMPERATURE(data_temperature);
+            printf("humedad %u temperature %i \n",humedity,temperature);
+            sleep(1);
+        }
+        close(my_dev);       
     }
     return 0;
-
-
 }
 
